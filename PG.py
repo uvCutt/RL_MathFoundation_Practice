985648246e5b234365dc950c7346a9d78d299d48
uvCutt <47786128+uvCutt@users.noreply.github.com>
2024-08-27 16:21:32 +0800

    Update PG.py
    更新PG

--- PG.py
+++ PG.py
@@ -17,19 +17,49 @@ class Params:
     # 动作空间和状态空间以及隐藏层大小
     action_space_size = 0
     state_space_size = 0
-    hidden_dim = 256
+    hidden_dim = 128
     # 学习率和折扣率, 很关键的参数!!! 调的时候一度怀疑自己的代码是否写错, 实际上就是参数问题
-    lr = 0.0005
+    lr = 0.0001
     gamma = 0.99
 
     # 训练次数以及每次最大步长
-    epochs = 2000
-    episode_len = 100000
+    epochs = 1000
+    episode_len = 200
 
+    # 经验回放池大小
+    replay_buffer_size = 10000
     batch_size = 64
 
     # 更新target的频率以及保存频率
     save_freq = 100
+
+
+class ReplayBuffer(object):
+    def __init__(self, capacity: int):
+        """
+        DQN贡献之一: 经验回访池, 打破数据时间相关性
+        通用经验回放池，利用队列来进行维护，先入先出
+        :param capacity: 经验回放池大小
+        """
+        self.capacity = capacity
+        self.buffer = deque(maxlen=self.capacity)
+
+    def push(self, transitions):
+        self.buffer.append(transitions)
+
+    def sample(self, batch_size: int) -> Tuple:
+        """
+        采样
+        :param batch_size: 样本数
+        :return: 采样结果
+        """
+        if batch_size > len(self.buffer):
+            batch_size = len(self.buffer)
+        batch = random.sample(self.buffer, batch_size)
+        return zip(*batch)
+
+    def __len__(self):
+        return len(self.buffer)
 
 
 class MLP(nn.Module):
@@ -46,12 +76,12 @@ class MLP(nn.Module):
     def forward(self, x):
         x = nn.ReLU()(self.input_linear(x))
         x = nn.ReLU()(self.hidden_linear(x))
-        x = nn.Softmax()(self.output_linear(x))
+        x = nn.Softmax(dim=-1)(self.output_linear(x))
         return x
 
 
-class DQN:
-    def __init__(self, params: Params = None, model: nn.Module = None):
+class PG:
+    def __init__(self, params: Params = None, model: nn.Module = None, replay_buffer: ReplayBuffer = None):
         self.params = params
 
         self.policy = model.to(self.params.device)
@@ -68,28 +98,21 @@ class DQN:
         :param rewards: 奖励
         :return: None
         """
-        states = np.array(states)
-        actions = np.array(actions)
-        rewards = np.array(rewards)
-
-        discounted_returns = []
+        states = torch.tensor(np.array(states)).to(self.params.device)
+        actions = torch.tensor(np.array(actions)).to(self.params.device)
         discounted_return = 0
-        for reward in rewards[::-1]:
-            discounted_return = reward + self.params.gamma * discounted_return
-            discounted_returns.append(discounted_return)
-        discounted_returns = torch.tensor(np.array(discounted_returns))
+        discounted_returns = []
+        for reward in rewards:
+            discounted_return = discounted_return * self.params.gamma + reward
+            discounted_returns.insert(0, discounted_return)
+        discounted_returns = torch.tensor(discounted_returns).to(self.params.device)
 
         self.optimizer.zero_grad()
+
         for t, (state, action) in enumerate(zip(states, actions)):
-            action_probs = self.policy(torch.tensor(state))
-            dist = torch.distributions.Categorical(action_probs)
-            # 这里没那么神秘，转换为对数概率计算更稳定，也可以自己用torch.log手动转试试看值是否一样
-            action_log_prob = dist.log_prob(torch.tensor(action))
+            action_log_prob = torch.distributions.Categorical(self.policy(state)).log_prob(action)
             loss = -action_log_prob * discounted_returns[t]
             loss.backward()
-
-        for param in self.policy.parameters():
-            param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
 
     def sample_action(self, state: np.ndarray) -> int:
@@ -98,10 +121,10 @@ class DQN:
         :param state: 当前状态
         :return: 执行的动作
         """
-        action_probs = self.policy(torch.tensor(state))
-        dist = torch.distributions.Categorical(action_probs)
+        state = torch.tensor(state).to(self.params.device)
+        dist = torch.distributions.Categorical(self.policy(state))
         action = dist.sample()
-        return action.item()
+        return action.detach().item()
 
 
 def smooth(data: np.ndarray, weight=0.9) -> list:
@@ -137,23 +160,28 @@ def env_seed(seed: int = 1) -> None:
     torch.backends.cudnn.enabled = False
 
 
-def train(params: Params = None, env: gym.Env = None, agent: DQN = None):
+def train(params: Params = None, env: gym.Env = None, agent: PG = None):
+    immediately_rewards = []
     for epoch in range(params.epochs):
         state, info = env.reset()
         states, actions, rewards = [], [], []
         for step in range(params.episode_len):
             action = agent.sample_action(state)
             next_state, reward, done, _, _ = env.step(action)
-            state = next_state
             states.append(state)
             actions.append(action)
             rewards.append(reward)
+
+            state = next_state
             if done:
-                agent.update(states, actions, rewards)
                 break
+
+        agent.update(states, actions, rewards)
+        immediately_rewards.append(sum(rewards))
+
         if not (epoch + 1) % params.save_freq:
-            print(f"回合:{epoch + 1}/{params.epochs}, 奖励:{sum(rewards):.2f}")
-            np.save(f"./data/pg_immediately_rewards{epoch + 1}.npy", np.array(rewards))
+            print(f"回合:{epoch + 1}/{params.epochs}, 平均奖励:{np.array(immediately_rewards).mean():.2f}")
+            np.save(f"./data/pg_immediately_rewards{epoch + 1}.npy", np.array(immediately_rewards))
             torch.save(agent.policy.state_dict(), f"./data/pg_policy_epoch{epoch + 1}.pt")
     env.close()
 
@@ -166,12 +194,12 @@ def pg():
     params.state_space_size = env.observation_space.shape[0]
 
     model = MLP(params)
-    agent = DQN(params, model)
+    agent = PG(params, model)
     train(params, env, agent)
 
 
 def plot_rewards():
-    data = np.load("./data/pg_immediately_rewards2000.npy")
+    data = np.load("./data/pg_immediately_rewards1000.npy")
     plt.xlabel("episodes")
     plt.ylabel("immediately_rewards")
     plt.plot(data, label='rewards')
@@ -182,4 +210,4 @@ def plot_rewards():
 
 if __name__ == "__main__":
     pg()
-    # plot_rewards()
+    plot_rewards()
